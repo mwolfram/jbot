be6fee7ced295d9a63aa7b3df9dfa4b9fadffab5
Máté Wolfram <m.wolfram@incubedit.com>
2018-03-04 11:55:52 +0100

    log pong message

--- jbot/src/main/java/me/ramswaroop/jbot/core/slack/BotWebSocketHandler.java
+++ jbot/src/main/java/me/ramswaroop/jbot/core/slack/BotWebSocketHandler.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.PongMessage;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
@@ -36,7 +37,12 @@ public class BotWebSocketHandler extends AbstractWebSocketHandler {
     protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws Exception {
         logger.error("Binary messages are not supported in Slack RTM API");
     }
-
+    
+    @Override
+    protected void handlePongMessage(WebSocketSession session, PongMessage message) throws Exception {
+        logger.debug("Pong message received");
+    }
+    
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         bot.afterConnectionClosed(session, status);
